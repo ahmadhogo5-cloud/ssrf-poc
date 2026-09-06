0ed2ece844c7602f208f9f6d36dda950ab3f7287
ahmadhogo5-cloud <ahmadhogo5@gmail.com>
2026-09-07 01:59:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/image.gif', (req, res) => {
-    // تأكد أن الرابط يبدأ بـ https:// بشكل صحيح
-    res.redirect(302, https://www.internalfb.com/intern/bug-bounty/get-canary-token/6c89420833684ad2be246b8e60b865a8/');
+    // تم إصلاح علامة الاقتباس المفقودة بالأسفل في بداية الرابط ونهايته
+    res.redirect(302, 'https://www.internalfb.com/intern/bug-bounty/get-canary-token/6c89420833684ad2be246b8e60b865a8/');
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
